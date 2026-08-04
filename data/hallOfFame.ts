08048d9414e46c202d0664b0e0ef92b8878452ef
PranavCM24032 <pranavpaunikar.ml24@sbjit.edu.in>
2026-08-04 10:24:16 +0530

    Update Hall of Fame with all images and proper alt text

--- data/hallOfFame.ts
+++ data/hallOfFame.ts
@@ -5,7 +5,14 @@ export interface HallOfFamePhoto {
 }
 
 export const HALL_OF_FAME_PHOTOS: HallOfFamePhoto[] = [
-  { id: "1", src: "/hall_of_fame/CSI_hack_1.jpeg", alt: "CSI Hackthon Hydrabad Chapter" },
-  { id: "2", src: "/hall_of_fame/CSI_hack_2.jpeg", alt: "CSI Hackthon Hydrabad Chapter Winners" },
-
+  { id: "1", src: "/hall_of_fame/CSI_Hack_1.jpeg", alt: "CSI Hackathon Hyderabad Chapter" },
+  { id: "2", src: "/hall_of_fame/CSI_hack_2.jpeg", alt: "CSI Hackathon Hyderabad Chapter Winners" },
+  { id: "3", src: "/hall_of_fame/ff_1.JPG", alt: "Flash Mob 1" },
+  { id: "4", src: "/hall_of_fame/ff_2.jpg", alt: "Flash Mob 2" },
+  { id: "5", src: "/hall_of_fame/ff_3.jpg", alt: "Flash Mob 3" },
+  { id: "6", src: "/hall_of_fame/ff_4.jpg", alt: "Flash Mob 4" },
+  { id: "7", src: "/hall_of_fame/ff_5.jpg", alt: "Flash Mob 5" },
+  { id: "8", src: "/hall_of_fame/fi_1.jpg", alt: "Forum Installation 1" },
+  { id: "9", src: "/hall_of_fame/fi_3 (1).jpg", alt: "Forum Installation 2" },
+  { id: "10", src: "/hall_of_fame/fi_3 (2).jpg", alt: "Forum Installation 3" },
 ];
